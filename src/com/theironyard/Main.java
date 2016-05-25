05208b6378ee9165633c2e628d7b503cb0c5e357
willriggins <williamriggins2013@gmail.com>
2016-05-25 12:57:28 -0400

    initial

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,11 @@
+package com.theironyard;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to the F/A-18 Quiz!\n");
+        System.out.println("");
+
+    }
+}
